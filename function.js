f2e78c47e32309665d4b5f6a6917cf519807147b
Salapille <noecool@outlook.fr>
2024-12-08 16:38:46 +0100

    creation du portfolio

---
+++ function.js
@@ -0,0 +1,10 @@
+// Fonction pour afficher un projet
+function openProject(projectName) {
+    alert('Vous avez cliqu� sur ' + projectName);
+}
+// Gestion du formulaire de contact
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Emp�che l'envoi r�el du formulaire
+    alert('Merci de nous avoir contact� !');
+    // Vous pouvez ajouter ici une logique d'envoi de formulaire r�el si n�cessaire
+});
